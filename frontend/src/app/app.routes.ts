1e06c75b01e6b82f9414013af56078f12c61acb1
Abderrahim Drissi <drissi.abderrahim@gmail.com>
2025-06-27 16:07:40 +0100

    remove entire route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,1 @@
-import { Routes } from '@angular/router';
-import { ConfigurationComponent } from './components/configuration/configuration.component';
 
-export const routes: Routes = [
-  { path: '', component: ConfigurationComponent }
-];
